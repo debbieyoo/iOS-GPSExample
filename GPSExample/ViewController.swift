8c28c6be10ebb07e7ffe015fea8cd1779d7fb921
Mark Sherriff <marksherriff@github.com>
2015-10-30 00:39:04 -0400

    initial commit

--- GPSExample/ViewController.swift
+++ GPSExample/ViewController.swift
@@ -7,19 +7,183 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+import CoreMotion
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager?
+    
+    lazy var motionManager = CMMotionManager()
+    var xDir: Double!
+    var yDir: Double!
+    var zDir: Double!
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var lat: UILabel!
+    @IBOutlet weak var lon: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count == 0{
+            //handle error here
+            return
+        }
+        
+        let newLocation = locations[0]
+        
+        print("Latitude = \(newLocation.coordinate.latitude)")
+        print("Longitude = \(newLocation.coordinate.longitude)")
+        lat.text = String(newLocation.coordinate.latitude)
+        lon.text = String(newLocation.coordinate.longitude)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(manager: CLLocationManager,
+        didFailWithError error: NSError){
+            print("Location manager failed with error = \(error)")
     }
+    
+    func locationManager(manager: CLLocationManager,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus){
+            
+            print("The authorization status of location services is changed to: ", terminator: "")
+            
+            switch CLLocationManager.authorizationStatus(){
+            case .AuthorizedAlways:
+                print("Authorized")
+            case .AuthorizedWhenInUse:
+                print("Authorized when in use")
+            case .Denied:
+                print("Denied")
+            case .NotDetermined:
+                print("Not determined")
+            case .Restricted:
+                print("Restricted")
+            }
+            
+    }
+    
+    func displayAlertWithTitle(title: String, message: String){
+        let controller = UIAlertController(title: title,
+            message: message,
+            preferredStyle: .Alert)
+        
+        controller.addAction(UIAlertAction(title: "OK",
+            style: .Default,
+            handler: nil))
+        
+        presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    
+    func createLocationManager(startImmediately startImmediately: Bool){
+        locationManager = CLLocationManager()
+        if let manager = locationManager{
+            print("Successfully created the location manager")
+            manager.delegate = self
+            if startImmediately{
+                manager.startUpdatingLocation()
+            }
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        /* Are location services available on this device? */
+        if CLLocationManager.locationServicesEnabled(){
+            
+            /* Do we have authorization to access location services? */
+            switch CLLocationManager.authorizationStatus(){
+            case .AuthorizedAlways:
+                /* Yes, always */
+                createLocationManager(startImmediately: true)
+            case .AuthorizedWhenInUse:
+                /* Yes, only when our app is in use */
+                createLocationManager(startImmediately: true)
+            case .Denied:
+                /* No */
+                displayAlertWithTitle("Not Determined",
+                    message: "Location services are not allowed for this app")
+            case .NotDetermined:
+                /* We don't know yet, we have to ask */
+                createLocationManager(startImmediately: false)
+                if let manager = self.locationManager{
+                    manager.requestWhenInUseAuthorization()
+                }
+            case .Restricted:
+                /* Restrictions have been applied, we have no access
+                to location services */
+                displayAlertWithTitle("Restricted",
+                    message: "Location services are not allowed for this app")
+            }
+            
+            
+        } else {
+            /* Location services are not enabled.
+            Take appropriate action: for instance, prompt the
+            user to enable the location services */
+            print("Location services are not enabled")
+        }
+    }
+    
+    // function to allow for detecting a shake
+    override func motionEnded(motion: UIEventSubtype,
+        withEvent event: UIEvent?) {
+            
+            if motion == .MotionShake{
+                let controller = UIAlertController(title: "Shake",
+                    message: "The device is shaken",
+                    preferredStyle: .Alert)
+                
+                controller.addAction(UIAlertAction(title: "OK",
+                    style: .Default,
+                    handler: nil))
+                
+                presentViewController(controller, animated: true, completion: nil)
+                
+            }
+            
+    }
+    @IBAction func startAccel(sender: UIButton) {
+        if motionManager.accelerometerAvailable{
+            let queue = NSOperationQueue()
+            motionManager.startAccelerometerUpdatesToQueue(queue, withHandler:
+                {data, error in
+                    
+                    guard let data = data else{
+                        return
+                    }
+                    
+                    print("X = \(data.acceleration.x)")
+                    print("Y = \(data.acceleration.y)")
+                    print("Z = \(data.acceleration.z)")
+                    self.xDir = data.acceleration.x
+                    self.yDir = data.acceleration.y
+                    self.zDir = data.acceleration.z
 
+                    
+                }
+            )
+        } else {
+            print("Accelerometer is not available")
+        }
+        
+        
+    }
+    @IBAction func checkDeviceRotation(sender: UIButton) {
+        xLabel.text = "x = " + String(xDir)
+        yLabel.text = "y = " + String(yDir)
+        zLabel.text = "z = " + String(zDir)
 
+    }
+        
+    
+    
 }
 
